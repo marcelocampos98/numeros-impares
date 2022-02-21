c9e366a134c590533fa60c0df3bf11f243500d03
marcelocampos98 <marcelocampos190098@gamil.com>
2022-02-20 21:18:32 -0400

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
+
+            int[] resultados = numeros.Where(x => (x % 2) != 0).ToArray();
+
+            Console.WriteLine("Números ímpares: ");
+
+            foreach (int item in resultados)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
